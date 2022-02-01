2466b9c7de4712e3e9a272268fc535c86b65f6b3
maxim-savchuk <maxsavchuk1105@gmail.com>
2022-02-01 15:16:35 +0200

    Add FindUs

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { SubHeading } from '../../components';
 
-const FindUs = () => (
-  <div>
-    FindUs
-  </div>
-);
+import { images } from '../../constants';
+
+const FindUs = () => {
+  return (
+    <div className='app__bg app__wrapper section__padding' id='contact'>
+      <div className='app__wrapper_info'>
+        <SubHeading title='Contact' />
+        <h2 className='headtext__cormorant' style={{ marginBottom: '3rem' }}>Find Us</h2>
+        <div className='app__wrapper-content'>
+          <p className='p__opensans'>Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 7SG</p>
+          <p className='p__cormorant' style={{ color: '#DCCA87', margin: '2rem 0' }}>Opening Hours</p>
+          <p className='p__opensans'>Mon - Fri: 10:00 am - 02:00 am</p>
+          <p className='p__opensans'>Sat - Sun: 10:00 am - 03:00 am</p>
+        </div>
+        <button className='custom__button' style={{ marginTop: '2rem' }}>Visit Us</button>
+      </div>
+
+      <div className='app__wrapper_img'>
+        <img src={images.findus} alt="Findus" />
+      </div>
+    </div>
+  )
+};
 
 export default FindUs;
